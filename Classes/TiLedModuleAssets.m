53360d8ddf2800f6f6776b967ff60780f73c5a33
Chris Magnussen <chris@appgutta.no>
2012-08-18 13:36:08 +0200

    Initial commit

---
+++ Classes/TiLedModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "TiLedModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation TiLedModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
